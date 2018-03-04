df0903e08d9c4c85b9d04f54f5f436c2758eb62a
nozagleh <nozagleh@gmail.com>
2018-03-04 22:03:33 +0100

    Add drink detail activity and layout file

---
+++ app/src/main/java/com/nozagleh/ormur/DrinkDetail.java
@@ -0,0 +1,13 @@
+package com.nozagleh.ormur;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrinkDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink_detail);
+    }
+}
